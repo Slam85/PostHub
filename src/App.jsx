731734d84505096bfa69da5e2364d4ed3e642fcb
Hel <lotophage@me.com>
2023-05-19 11:07:11 +0200

    connexion dash pref

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Dashboard from "../Creation/Dashboard";
+import Preferences from "../Creation/Preferences";
+import Connexion from "../Connexion/Connexion";
+import {BrowerRouter, Route, Switch} from "react-router-dom";
 
 function App() {
+    const [token, setToken] = useState();
+    if(!token) {
+        return <Connexion setToken={setToken}/>;
+    }
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+            <Switch>
+                <Route path="/dashboard">
+                    <Dashboard/>
+                    <Route path="/preferences"/>
+                    <Preferences/>
+                    </Route>
+                    </Switch>
+                    </BrowerRouter>
+                    </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/Connexion/Connexion.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Connexion (){ 
+    return (
+        <div className="login-wrapper">
+            <h1>Connexion</h1>
+        <form>
+            <label>
+                <p>Username</p>
+                <input type="text"/>
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password"/>
+                        </label>
+                        <div>
+                            <button type="submit">Submit</button>
+                            </div>
+                            </form>
+                            </div>
+    )
+}
+
+export default Connexion;
