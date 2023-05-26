77b3528941d8806939450e33e7f949462f3b9944
Hel <lotophage@me.com>
2023-05-26 09:29:45 +0200

    commentaires error 404

--- src/Components/Nav/Error.jsx
+++ src/Components/Nav/Error.jsx
@@ -7,22 +7,26 @@ import "../Layouts/Error.css";
 function Error() {
   return (
     <div>
+
+      // RAPPEL DE LA BARRE DE NAVIGATION
       <NavBar />
       <div className="home">
         <section class="page_404">
           <div class="container">
             <div class="row">
               <div class="col-sm-12 ">
-                <div class="col-sm-10 col-sm-offset-1  text-center">
+                <div class="col-sm-10 col-sm-offset-1 text-center">
                   <div class="four_zero_four_bg">
                     <h1 class="text-center ">404</h1>
                   </div>
 
                   <div class="contant_box_404">
-                    <h3 class="h2">Look like you're lost</h3>
+
+                    <h3 class="h2">Looks like you're lost</h3>
 
                     <p>The page you are looking for is not avaible!</p>
 
+                    // LIEN DE REDIRECTION VERS LA PAGE D'ACCUEIL
                     <a href="./Home" class="link_404">
                       Go to Home
                     </a>
